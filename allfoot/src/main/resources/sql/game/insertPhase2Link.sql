4c70641bab21e6fc079f4fa22d98feb9136b4ff5
Po-haoHuang <leohuang131@gmail.com>
2017-09-05 15:14:53 +0800

    update link
    Change-Id: I506e33b3b782ddbced805f8202aee07db5ad4faf

--- allfoot/src/main/resources/sql/game/insertPhase2Link.sql
+++ allfoot/src/main/resources/sql/game/insertPhase2Link.sql
@@ -1,2 +1,2 @@
-INSERT INTO room_link (room_id_from, room_id_to, distance) VALUES (5, 11, 300);
-INSERT INTO room_link (room_id_from, room_id_to, distance) VALUES (10, 11, 300);
+INSERT INTO room_link (room_id_from, room_id_to, distance) VALUES (5, 11, 10);
+INSERT INTO room_link (room_id_from, room_id_to, distance) VALUES (10, 11, 10);
